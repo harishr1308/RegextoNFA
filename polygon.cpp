b26559fdb9edbb03a87cd6cf5cfc127856ef5375
busrikanta <busrikanta@outlook.com>
2016-10-13 17:26:27 +0530

    User Input

--- polygon.cpp
+++ polygon.cpp
@@ -438,6 +438,8 @@ void keyboard(unsigned char key, int x, int y)
 
 int main(int argc, char** argv)
 {
+cout<<"Enter the string: ";
+  cin>>inre;
  glutInit(&argc,argv);
  glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
  glutInitWindowPosition(0,0);
