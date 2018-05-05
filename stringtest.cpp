26ebd3b5c32e65d7bf1bddeedb0bd7e7c58ef7b9
harishr1308 <harishr1308@gmail.com>
2018-05-05 20:17:10 +0530

    Fixed basic_string<char> to char error

--- stringtest.cpp
+++ stringtest.cpp
@@ -1,14 +1,21 @@
 #include<string>
 #include<iostream>
+
 using namespace std;
+
 int main()
 {
-char s="s";
-for (int i=0;i<3;i++)
-  {
-   char ch=char(i+48);
-   char text=s+ch;
-   cout<<text<<endl;
-  }
-return 0;
+	string s="s";
+	for (int i=0;i<3;i++)
+	{
+		// returns the characters 0,1 and 2
+	   	char ch=char(i+48);
+
+	   	// concat between string and char
+	   	string text=s+ch;
+		
+		// this line prints s0,s1 and s2
+	   	cout<<text<<endl;
+	}
+	return 0;
 }
